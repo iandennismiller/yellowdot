1694104301cfdb0406ae36c230479556a5c9fd66
Ian Dennis Miller <iandennismiller@gmail.com>
2011-04-23 13:18:54 -0400

    * trying pod documentation instead of markdown

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,17 @@
+import os
+
+def docs():
+    css_url = "http://iandennismiller.com/css/perl_pod.css"
+    cmd = 'pod2html --css "%s" --outfile index.html --infile bin/yellowdot.pl; rm pod2*' % css_url
+    os.system(cmd)
+
+    #pod2html --css "http://iandennismiller.com/css/perl_pod.css" --outfile $(MAKE_PATH)/tmp/index.html \
+    #    --infile $(MAKE_PATH)/bin/yellowdot; rm $(MAKE_PATH)/pod2htmd.tmp $(MAKE_PATH)/pod2htmi.tmp
 
 
 """
 # create .html documentation
 
-    pod2html --css "http://iandennismiller.com/css/perl_pod.css" --outfile $(MAKE_PATH)/tmp/index.html \
-        --infile $(MAKE_PATH)/bin/yellowdot; rm $(MAKE_PATH)/pod2htmd.tmp $(MAKE_PATH)/pod2htmi.tmp
 
 # copy index.html
 
